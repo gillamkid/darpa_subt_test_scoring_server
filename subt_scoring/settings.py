05ed6b40171aef4b7e51fda65184efee0c7daa95
Sam Gillam <gillamkid@gmail.com>
2019-06-13 13:23:17 -0600

    replaced postgres with sqlite

--- subt_scoring/settings.py
+++ subt_scoring/settings.py
@@ -94,11 +94,8 @@ WSGI_APPLICATION = 'subt_scoring.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'postgres',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
